8711b350a2a0c40e2dc9b071a328738cd798b1a2
khalil ben khoud <khalil.b.khoud1998@gmail.com>
2024-12-02 21:44:58 +0100

    fixed chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { MessageService } from 'primeng/api';
 import { AuthService } from '../services/auth.service';
 import { animations } from '../utils/animations';
 import { PortfolioService } from '../services/portfolio.service'; // Import PortfolioService
+import { Chart } from 'chart.js/auto'
 
 
 
@@ -27,7 +28,52 @@ export class dashboardComponent implements OnInit{
 
   ngOnInit(): void {
     this.getPortfolioBalance();  // Fetch balance on initialization
+    
   }
+
+  ngAfterViewInit() : void {
+    this.createPieChart() ;
+  }
+  createPieChart() {
+    const ctx = document.getElementById('myPieChart') as HTMLCanvasElement;
+
+    new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top'
+          }
+        }
+      }
+    });
+  }
+
 
   getPortfolioBalance(): void {
     this.portfolioService.getBalance().subscribe(
